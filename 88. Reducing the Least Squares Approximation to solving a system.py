833c54e23249b79cafb54458214f779eefdd8fb2
Seng Keat <corodostudio@gmail.com>
2023-08-01 21:31:02 +0700

    Reducing the least square appro solving a system

---
+++ 88. Reducing the Least Squares Approximation to solving a system.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+def least_squares_approximation(x, y):
+  """
+  Performs least squares approximation on the data points (x, y).
+
+  Args:
+    x: The x-coordinates of the data points.
+    y: The y-coordinates of the data points.
+
+  Returns:
+    m: The slope of the least squares line.
+    b: The y-intercept of the least squares line.
+  """
+
+  n = len(x)
+  A = np.zeros((n, 2))
+  b = np.zeros(n)
+
+  for i in range(n):
+    A[i, 0] = x[i]
+    A[i, 1] = 1
+    b[i] = y[i]
+
+  m, b = np.linalg.lstsq(A, b)[0]
+
+  return m, b
+
+if __name__ == "__main__":
+  x = np.array([1, 2, 3, 4, 5])
+  y = np.array([2, 4, 6, 8, 10])
+  m, b = least_squares_approximation(x, y)
+  print(m, b)
