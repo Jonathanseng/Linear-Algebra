5d2c83d63245c72a4d9ac6041d7f8ff563bb1ab9
Seng Keat <corodostudio@gmail.com>
2023-08-01 08:16:03 +0700

    Span is a subspaces

---
+++ 49. span is subspaces.py
@@ -0,0 +1,64 @@
+def is_span(S):
+  """
+  Returns True if S is the span of a set of vectors in R^n, False otherwise.
+
+  Args:
+    S: A set of vectors in R^n.
+
+  Returns:
+    True if S is the span of a set of vectors in R^n, False otherwise.
+  """
+
+  if not S:
+    return True
+
+  for vector in S:
+    if vector == 0:
+      return False
+
+  for vector1 in S:
+    for vector2 in S:
+      if vector1 + vector2 not in S:
+        return False
+
+  return True
+
+
+def is_subspace(S):
+  """
+  Returns True if S is a subspace of R^n, False otherwise.
+
+  Args:
+    S: A set of vectors in R^n.
+
+  Returns:
+    True if S is a subspace of R^n, False otherwise.
+  """
+
+  if not S:
+    return True
+
+  for vector in S:
+    if vector == 0:
+      return False
+
+  for vector1 in S:
+    for vector2 in S:
+      if vector1 + vector2 not in S:
+        return False
+
+  return True
+
+
+def main():
+  S = {(1, 2), (3, 4), (5, 6)}
+  print(is_span(S))
+  # Output: True
+
+  S = {(1, 2), (3, 4), (0, 0)}
+  print(is_span(S))
+  # Output: False
+
+
+if __name__ == "__main__":
+  main()
